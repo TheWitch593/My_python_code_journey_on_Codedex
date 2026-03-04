cd1656d59f684d3e959052bceeade08b518705cf
Bruna Dragulin <dragulinbruna2004@gmail.com>
2026-03-04 11:58:36 +0200

    Fix indentation in blood_moon function for consistency

--- Daily_Challange/day2.py
+++ Daily_Challange/day2.py
@@ -3,10 +3,10 @@ def blood_moon(time):
  t = time.split(':')
  totalh = (int(t[0])*60)+int(t[1]) # Convert time to total minutes
  for i in range(3):
-   totalh = totalh + 168  # 2h 48min in minutes
-   current_time_in_minutes = totalh % 1440 # 1440 minutes in a day
-    h = current_time_in_minutes // 60 # Get hours
-    m = current_time_in_minutes % 60 # Get minutes
-    h1 = f"{h:02}:{m:02}" # Format time as HH:MM
-    hours.append(h1)
+  totalh = totalh + 168  # 2h 48min in minutes
+  current_time_in_minutes = totalh % 1440 # 1440 minutes in a day
+  h = current_time_in_minutes // 60 # Get hours
+  m = current_time_in_minutes % 60 # Get minutes
+  h1 = f"{h:02}:{m:02}" # Format time as HH:MM
+  hours.append(h1)
  return hours
